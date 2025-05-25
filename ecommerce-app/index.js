72c467ce1598526b506d2df85d4fcc68bec7adf8
VishalPcode <vishalgupta732160@gmail.com>
2025-05-25 20:15:49 +0530

    Fun with express

---
+++ ecommerce-app/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+     res.send('Hello World!')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${process.env.port}`)
+})
